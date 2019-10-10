8a82a0961cdef64893de6bc6b22444c0b4cf40a4
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-10-10 17:00:17 +0200

    Add playerstat component

---
+++ src/components/Playerstat.vue
@@ -0,0 +1,40 @@
+<template>
+	<v-card>
+		<v-card v-for="(item, index) in $store.getters.statPlayer" :key="index" dark class="mb-4">
+			<v-list-item>
+				<v-list-item-avatar size="60">
+					<v-img :src="item.avatar" class="elevation-6"></v-img>
+				</v-list-item-avatar>
+				<v-list-item-content>
+					<v-list-item-title class="headline">{{ item.name }}</v-list-item-title>
+					<!-- <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle> -->
+				</v-list-item-content>
+			</v-list-item>
+
+			<v-card-text>
+				<v-simple-table dark>
+					<template v-slot:default>
+						<tbody>
+							<tr>
+								<td class="text-left">Victoires</td>
+								<td>{{ item.win }}</td>
+							</tr>
+							<tr>
+								<td class="text-left">Nuls</td>
+								<td>{{ item.draw }}</td>
+							</tr>
+							<tr>
+								<td class="text-left">Défaites</td>
+								<td>{{ item.lose }}</td>
+							</tr>
+							<tr>
+								<td class="text-left">Différence</td>
+								<td>{{ item.diff }} ({{ item.goals }}/{{ item.goalsagainst }})</td>
+							</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+			</v-card-text>    
+		</v-card>
+	</v-card>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,55 +7,17 @@
         order="2"
         order-md="1"
       >
-        <list :scores = "scores" />
+        <List :scores = "scores" />
       </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-          order="1"
-          order-md="2"
-        >
-          <v-card v-for="(item, index) in $store.getters.statPlayer" :key="index" dark class="mb-4">
-
-            <v-list-item>
-              <v-list-item-avatar size="60">
-                <v-img :src="item.avatar" class="elevation-6"></v-img>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">{{ item.name }}</v-list-item-title>
-                <!-- <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle> -->
-              </v-list-item-content>
-            </v-list-item>
-
-    <v-card-text>
-      <v-simple-table dark>
-        <template v-slot:default>
-          <tbody>
-            <tr>
-              <td class="text-left">Victoires</td>
-              <td>{{ item.win }}</td>
-            </tr>
-            <tr>
-              <td class="text-left">Nuls</td>
-              <td>{{ item.draw }}</td>
-            </tr>
-            <tr>
-              <td class="text-left">Défaites</td>
-              <td>{{ item.lose }}</td>
-            </tr>
-            <tr>
-              <td class="text-left">Différence</td>
-              <td>{{ item.diff }} ({{ item.goals }}/{{ item.goalsagainst }})</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-card-text>
-
-             
-          </v-card>
-        </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        order="1"
+        order-md="2"
+      >
+        <Playerstat />
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -63,6 +25,7 @@
 <script>
 
 import List from '@/components/List'
+import Playerstat from '@/components/Playerstat'
 
 import { mapState, mapGetters } from 'vuex'
 
@@ -73,10 +36,12 @@ export default {
     }
   },
   components: {
-    List
+    List, Playerstat
   },
-  computed: mapState(['scores']),
-
+  computed: {
+    ...mapState(['scores']),
+    // ...mapGetters(['playerStat'])
+  },
   mounted() {
     this.$store.dispatch('bindScores')
   }
